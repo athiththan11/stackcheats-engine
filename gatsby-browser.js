7db2ff627a964be4bb83f78b0a6518a5344f0582
athiththan11 <code19111995@gmail.com>
2019-07-13 11:50:38 +0530

    styles update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,5 @@ require('prismjs/themes/prism-solarizedlight.css')
 require('prismjs/plugins/line-numbers/prism-line-numbers.css')
 
 import './src/styles/sidebar-menu.css'
+import './src/styles/styles.css'
 // import './src/styles/gatsby.css'
